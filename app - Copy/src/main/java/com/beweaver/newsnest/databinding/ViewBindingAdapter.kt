0d0b607b7685cd5340e45fcdbc60d3fd6546cd0b
Sandeep kumar Polu <sandeep.chennur@gmail.com>
2024-02-03 19:11:55 -0800

    Deleted duplicate copy
    Deleted duplicate copy

--- app - Copy/src/main/java/com/beweaver/newsnest/databinding/ViewBindingAdapter.kt
+++
@@ -1,9 +0,0 @@
-package com.beweaver.newsnest.databinding
-
-import android.view.View
-import androidx.databinding.BindingAdapter
-
-@BindingAdapter("app:visibility")
-fun View.bindVisibility(value: Boolean) {
-    visibility = if (value)  View.VISIBLE else View.GONE
-}
